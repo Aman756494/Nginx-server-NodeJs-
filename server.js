233bd147c30f5c88e5140c1a2c942ebcbdc05103
Aman756494 <kraman9262@gmail.com>
2026-07-21 16:16:48 +0530

    create file path and extension Name

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const port = 3000;
 
-const server = http.createServer();
+const server = http.createServer((req , res) => {
+    const filePath = path.join(__dirname , req.url === '/' ? "index.html" : "req.url")
+
+    const extName = String(path.extname(filePath)).toLowerCase();
+});
 
 server.listen(port, () => {
     console.log(`Server is listening to the port ${port}`);
